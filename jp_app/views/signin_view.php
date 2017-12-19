9938f12bd2a0e823ac6d28adf178db7578cc25f1
cosmas28 <augubilla100@gmail.com>
2017-12-20 02:18:14 +0300

    added login form to signin view

--- jp_app/views/signin_view.php
+++ jp_app/views/signin_view.php
@@ -14,6 +14,43 @@
         <?php $this->load->view('base/dashboard_header'); ?>
         <!-- #header end -->
 
+        <!-- Content
+		============================================= -->
+        <section id="content">
+            <div class="content-wrap nopadding">
+                <div class="section nopadding nomargin" style="width: 100%; height: 100%; position: absolute; left: 0; top: 0; background: #444;"></div>
+
+                <div class="section nobg full-screen nopadding nomargin">
+                    <div class="container vertical-middle divcenter clearfix">
+
+                        <div class="panel panel-default divcenter noradius noborder" style="max-width: 400px;">
+                            <div class="panel-body" style="padding: 40px;">
+                                <form id="login-form" name="login-form" class="nobottommargin" action="#" method="post">
+                                    <h3>Login to your Account</h3>
+
+                                    <div class="col_full">
+                                        <label for="login-form-username">Username:</label>
+                                        <input type="text" id="login-form-username" name="login-form-username" value="" class="form-control not-dark" />
+                                    </div>
+
+                                    <div class="col_full">
+                                        <label for="login-form-password">Password:</label>
+                                        <input type="password" id="login-form-password" name="login-form-password" value="" class="form-control not-dark" />
+                                    </div>
+
+                                    <div class="col_full nobottommargin">
+                                        <button class="button button-3d button-black nomargin" id="login-form-submit" name="login-form-submit" value="login">Login</button>
+                                        <a href="#" class="fright">Forgot Password?</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content -->
+
         <!-- Footer
 		============================================= -->
         <?php $this->load->view('base/footer'); ?>
