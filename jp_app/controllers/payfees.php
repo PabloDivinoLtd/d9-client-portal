404a2bfb51d5a01e0f5abe869ee0d91c3cf79512
cosmas28 <augubilla100@gmail.com>
2017-12-21 17:22:24 +0300

    add changes

--- jp_app/controllers/payfees.php
+++ jp_app/controllers/payfees.php
@@ -21,53 +21,63 @@ class Payfees extends CI_Controller {
         $data['title'] = 'D9 Pay Fees';
         $data['msg']='';
 
-        $this->form_validation->set_message('is_unique', 'The %s is already taken');
-
-        if (empty($_FILES['slip_file']['name']))
+        if (empty($_FILES['slip_file']['name'])){
             $this->form_validation->set_rules('slip_file', 'Receipt', 'required');
+        }
 
         $this->form_validation->set_error_delimiters('<div class="errowbox"><div class="erormsg">', '</div></div>');
         if ($this->form_validation->run() === FALSE) {
             $this->load->view('payfees_view',$data);
             return;
+        } else
+            if (!empty($_FILES['slip_file']['name'])){
 
-        }
-        if (!empty($_FILES['slip_file']['name'])){
+                $extention = get_file_extension($_FILES['cv_file']['name']);
+                $allowed_types = array('doc','docx','pdf','rtf','jpg','txt');
 
-            $extention = get_file_extension($_FILES['cv_file']['name']);
-            $allowed_types = array('doc','docx','pdf','rtf','jpg','txt');
+                if(!in_array($extention,$allowed_types)){
+                    $this->session->set_flashdata('msg', '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong>Invalid file format.</div>');
+                    $this->load->view('payfees_view',$data);
+                    return;
+                }
 
-            if(!in_array($extention,$allowed_types)){
-                $this->session->set_flashdata('msg', '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Error!</strong>Invalid file format.</div>');
-                $this->load->view('payfees_view',$data);
-                return;
+                #$receipt = $this->input->$_FILES['slip_file'];
+                $real_path = realpath(APPPATH . '../public/uploads/');
+                $config['upload_path'] = $real_path;
+                $config['allowed_types'] = 'doc|docx|pdf|rtf|jpg|txt';
+                $config['overwrite'] = true;
+                $config['max_size'] = 6000;
+                #$config['file_name'] = replace_string(' ','-',strtolower('d9').'-'.$seeker_id;
+                $this->upload->initialize($config);
+                if (!$this->upload->do_upload('slip_file')){
+                    $data['msg'] = $this->upload->display_errors();
+                    $this->load->view('payfees_view',$data);
+                    return;
+                }
+
+                $receipt = array('upload_data' => $this->upload->data());
+
             }
 
-            $receipt = $this->input->$_FILES['slip_file'];
+        $config = array();
+        $config['smtp_host'] = 'ssl://smtp.googlemail.com';
+        $config['smtp_port'] = 465;
+        $config['smtp_user'] = 'augubilla100@gmail.com';
+        $config['smtp_pass'] = 'mbegebillal+100';
+        $config['smtp_crypto'] = 'tls';
+        $config['wordwrap'] = TRUE;
+        $config['mailtype'] = 'html';
 
-            $config = array();
-            $config['smtp_host'] = 'ssl://smtp.googlemail.com';
-            $config['smtp_port'] = 465;
-            $config['smtp_user'] = 'augubilla100@gmail.com';
-            $config['smtp_pass'] = 'mbegebillal+100';
-            $config['smtp_crypto'] = 'tls';
-            $config['wordwrap'] = TRUE;
-            $config['mailtype'] = 'html';
+        $this->email->initialize($config);
+        $this->email->clear(TRUE);
+        $this->email->from($this->session->userdata('user_email'));
+        $this->email->to('augubilla100@gmail.com');
 
-            $this->email->initialize($config);
-            $this->email->clear(TRUE);
-            $this->email->from($this->session->userdata('user_email'));
-            $this->email->to('augubilla100@gmail.com');
-            //
-            $this->email->subject('Creditor payment');
-            $mail_message = 'The attached file have the creditor receipt';
-            $this->email->attach($receipt);
-            $this->email->message($mail_message);
+        $this->email->subject('Creditor payment');
+        $mail_message = 'The attached file have the creditor receipt';
+        $this->email->attach($receipt);
+        $this->email->message($mail_message);
 
-            redirect(base_url('pay_fees'));
-        }
-
-        $this->load->view('payfees_view', $data);
-
+        redirect(base_url('pay_fees'));
     }
 }
--- jp_app/views/payfees_view.php
+++ jp_app/views/payfees_view.php
@@ -20,29 +20,29 @@
         <div class="content-wrap">
             <div class="container clearfix">
                 <div class="tabs divcenter nobottommargin clearfix" id="tab-login-register" style="max-width: 500px;">
-                    <h2>Upload the paid receipt.</h2>
-                    <div class="row center">
-                        <a href="index.html"><img src="<?php echo base_url('public/images/barcode.png');?>" alt="Canvas Logo"></a>
-                    </div class="row center">
-                        <div style="font-size: 24px;font-weight: 700;">Pay to Bitcoin address :</div>
-                        <p>1FtuDqexFsLktYvEc5TVxje2i1svAsdTpU</p>
-                    <div >
+                    <div class="row">
+                        <?php echo form_open_multipart('payfees',array('name' => 'payfees_form', 'id' => 'payfees_form', 'onSubmit' => 'return validate_form(this);'));?>
+                        <h2>Upload the paid receipt.</h2>
+                        <?php echo $this->session->flashdata('msg');?>
+                        <div class="row center">
+                            <img src="<?php echo base_url('public/images/barcode.png');?>" alt="Bar code">
+                        </div>
+                            <div style="font-size: 24px;font-weight: 700;">Pay to Bitcoin address :</div>
+                            <p>1FtuDqexFsLktYvEc5TVxje2i1svAsdTpU</p>
+                            <div class="col_full <?php echo (form_error('slip_file') || $msg)?'has-error':'';?>">
+                                <label></label><br>
+                                <input type="file" name="slip_file" id="slip_file" value="<?php echo set_value('slip_file'); ?>" class="file">
+                                <?php
+                                echo form_error('slip_file');
+                                echo ($msg!='')?'<div class="alert alert-error"> <a class="close" data-dismiss="alert">×</a>'.$msg.'</div>':'';
+                                ?>
+                            </div>
 
+                            <div class="col_full nobottommargin">
+                                <button class="button button-3d button-black nomargin" type="submit" id="submit" name="submit" >Upload your payment receipt</button>
+                            </div>
+                        <?php echo form_close();?>
                     </div>
-                    <?php echo form_open_multipart('payfees',array('name' => 'payfees_form', 'class' => 'nobottommargin', 'id' => 'payfees_form', 'onSubmit' => 'return validate_form(this);'));?>
-                        <div class="col_full <?php echo (form_error('slip_file') || $msg)?'has-error':'';?>">
-                            <label></label><br>
-                            <input type="file" name="slip_file" id="slip_file" value="<?php echo set_value('slip_file'); ?>" class="file">
-                            <?php
-                            echo form_error('slip_file');
-                            echo ($msg!='')?'<div class="alert alert-error"> <a class="close" data-dismiss="alert">×</a>'.$msg.'</div>':'';
-                            ?>
-                        </div>
-
-                        <div class="col_full nobottommargin">
-                            <button class="button button-3d button-black nomargin" type="submit" id="submit" name="submit" value="register">Upload your payment receipt</button>
-                        </div>
-                    <?php echo form_close();?>
                 </div>
             </div>
         </div>
@@ -59,68 +59,6 @@
 ============================================= -->
 <div id="gotoTop" class="icon-angle-up"></div>
 <?php $this->load->view('base/before_body_close'); ?>
-<script  type="text/javascript">
-    $(document).on('ready', function() {
-        $("#input-5").fileinput({showCaption: false});
 
-        $("#input-6").fileinput({
-            showUpload: false,
-            maxFileCount: 10,
-            mainClass: "input-group-lg",
-            showCaption: true
-        });
-
-        $("#input-8").fileinput({
-            mainClass: "input-group-md",
-            showUpload: true,
-            previewFileType: "image",
-            browseClass: "btn btn-success",
-            browseLabel: "Pick Image",
-            browseIcon: "<i class=\"icon-picture\"></i> ",
-            removeClass: "btn btn-danger",
-            removeLabel: "Delete",
-            removeIcon: "<i class=\"icon-trash\"></i> ",
-            uploadClass: "btn btn-info",
-            uploadLabel: "Upload",
-            uploadIcon: "<i class=\"icon-upload\"></i> "
-        });
-
-        $("#input-9").fileinput({
-            previewFileType: "text",
-            allowedFileExtensions: ["txt", "md", "ini", "text", "pdf"],
-            previewClass: "bg-warning",
-            browseClass: "btn btn-primary",
-            removeClass: "btn btn-default",
-            uploadClass: "btn btn-default",
-        });
-
-        $("#input-10").fileinput({
-            showUpload: false,
-            layoutTemplates: {
-                main1: "{preview}\n" +
-                "<div class=\'input-group {class}\'>\n" +
-                "   <div class=\'input-group-btn\'>\n" +
-                "       {browse}\n" +
-                "       {upload}\n" +
-                "       {remove}\n" +
-                "   </div>\n" +
-                "   {caption}\n" +
-                "</div>"
-            }
-        });
-
-        $("#input-11").fileinput({
-            maxFileCount: 10,
-            allowedFileTypes: ["image", "video"]
-        });
-
-        $("#input-12").fileinput({
-            showPreview: false,
-            allowedFileExtensions: ["zip", "rar", "gz", "tgz"],
-            elErrorContainer: "#errorBlock"
-        });
-    });
-
-</script>
 </body>
 </html>
